e54ad4d7c608278ba7c22ed3059c8d305ec9b5a3
Thomas <114309234+thomasweigert91@users.noreply.github.com>
2026-07-22 12:40:39 +0200

    feat: defined types
    Defined types for user, question and answer

---
+++ src/types/question.ts
@@ -0,0 +1,10 @@
+import type { Answer } from "./answer";
+
+export type Question = {
+  readonly id: string;
+  title: string;
+  body: string;
+  createdAt: Date;
+  createdBy: string;
+  answers: Answer[];
+};
