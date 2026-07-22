02d55f9c073363acb00c6caed6ad30334bad1b3c
Thomas <114309234+thomasweigert91@users.noreply.github.com>
2026-07-22 12:51:12 +0200

    feat: add mockdata, improve types
    Added mockdata, improved current types

---
+++ src/data/users.mock.ts
@@ -0,0 +1,20 @@
+import type { User } from "../types/user";
+
+export const USERS: User[] = [
+  {
+    id: "user-1",
+    username: "codeExplorer",
+  },
+  {
+    id: "user-2",
+    username: "typescriptTom",
+  },
+  {
+    id: "user-3",
+    username: "frontendFox",
+  },
+  {
+    id: "user-4",
+    username: "backendBee",
+  },
+];
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,9 +1,4 @@
-import type { Answer } from "./answer";
-import type { Question } from "./question";
-
 export type User = {
   readonly id: string;
   username: string;
-  questions: Question[];
-  answers: Answer[];
 };
